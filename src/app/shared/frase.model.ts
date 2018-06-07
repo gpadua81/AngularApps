34fc8375a86e78bffd68c9377c9e34185b27814b
Gustavo Cortinhas De Padua <gdepadua@na.ad.tullib.com>
2018-06-07 19:40:19 -0300

    Test

---
+++ src/app/shared/frase.model.ts
@@ -0,0 +1,14 @@
+export class Frase
+{
+    constructor(public fraseEng: string, public frasePtBr: string){}
+    /*
+    public fraseEng: string
+    public frasePtBr: string
+
+    constructor(fraseEng: string, frasePtBr: string){
+        this.fraseEng = fraseEng
+        this.frasePtBr = frasePtBr
+
+    }
+    */
+}
