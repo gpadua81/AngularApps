378e4f96540f7f64852d7260c48b95e248306db9
Gustavo Cortinhas De Padua <gdepadua@na.ad.tullib.com>
2018-06-07 20:38:05 -0300

    Atualizaçõe de Progresso

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -9,16 +9,39 @@ import { FRASES } from './frase-mock'
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-  
-  public frases : Frase[] = FRASES
+    
   public instrucao: string = 'Traduza a frase:'
-  constructor() { console.log(this.frases) }
+  public resposta: string
+  
+  public frases : Frase[] = FRASES  
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  public progresso : number = 0
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase) 
+  }
 
   ngOnInit() {
   }
 
-  public atualizaResposta(resposta: Event): void{
-    console.log(resposta.target.value)
+  public atualizaResposta(resposta: Event): void{    
+    this.resposta = (<HTMLInputElement>resposta.target).value
   }
 
+  public verificarResposta(): void{
+    //console.log('Verificar resposta: ', this.resposta)
+
+    if(this.rodadaFrase.frasePtBr == this.resposta){
+      alert('A tradução está correta')        
+      this.rodada++
+      this.progresso += (100 / this.frases.length)      
+      this.rodadaFrase = this.frases[this.rodada]
+      
+    } else{
+      alert('A tradução está errada')
+    }    
+  }
 }
